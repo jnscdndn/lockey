0688390d068a2aebf49ce4af30a3c32c9df0cf57
Jyotiranjan sankhua <jyoti.sankhua@gmail.com>
2024-10-13 22:01:55 +0530

    some changes

--- js/index.js
+++ js/index.js
@@ -1,16 +1,21 @@
 let isNavOpen = false
 $("#mobile-nav-icon-holder").click(function(){
-    $("#nav-bar-contents-mobile").slideToggle();
+    
     if(isNavOpen){
-        $("#top-nav-bar-mobile").removeClass("bg-pink");
-        $("#mobile-nav-icon-holder div").addClass("bg-pink")
-        $("#mobile-nav-icon-holder div").removeClass("bg-beige")
-        isNavOpen = false
+        $("#nav-bar-contents-mobile").slideToggle(function(){
+            $("#top-nav-bar-mobile").removeClass("bg-pink");
+            $("#mobile-nav-icon-holder div").addClass("bg-pink")
+            $("#mobile-nav-icon-holder div").removeClass("bg-beige")
+            isNavOpen = false
+        });
     }
     else{
+        $("#nav-bar-contents-mobile").slideToggle()
         $("#top-nav-bar-mobile").addClass("bg-pink");
         $("#mobile-nav-icon-holder div").removeClass("bg-pink")
         $("#mobile-nav-icon-holder div").addClass("bg-beige")
         isNavOpen = true
     }
 })
+
+
