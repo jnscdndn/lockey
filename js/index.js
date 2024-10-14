595556e5601a55ec094f67bb02af8115662bf336
Jyotiranjan sankhua <jyoti.sankhua@gmail.com>
2024-10-14 06:20:02 +0530

    added swipe up using fingers

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 let isNavOpen = false
+let menu=$("#nav-bar-contents-mobile")
 $("#mobile-nav-icon-holder").click(function(){
     if(isNavOpen){
-        $("#nav-bar-contents-mobile").slideToggle(function(){
+        menu.slideToggle(function(){
             $("#top-nav-bar-mobile").removeClass("bg-pink");
             $("#mobile-nav-icon-holder div").addClass("bg-pink")
             $("#mobile-nav-icon-holder div").removeClass("bg-beige")
@@ -9,12 +10,28 @@ $("#mobile-nav-icon-holder").click(function(){
         });
     }
     else{
-        $("#nav-bar-contents-mobile").slideToggle()
-        $("#top-nav-bar-mobile").addClass("bg-pink");
+        menu.slideToggle()
+        $("#top-nav-bar-mobile").addClass("bg-pink")
         $("#mobile-nav-icon-holder div").removeClass("bg-pink")
         $("#mobile-nav-icon-holder div").addClass("bg-beige")
         isNavOpen = true
     }
 })
+let startY=0
+let endY=0
+menu.on('touchstart',function(e){
+    startY=e.originalEvent.touches[0].clientY
+})
 
+menu.on('touchmove',function(e){
+    endY=e.originalEvent.touches[0].clientY
+})
 
+menu.on('touchend',function(){
+    menu.slideToggle(function(){
+        $("#top-nav-bar-mobile").removeClass("bg-pink");
+        $("#mobile-nav-icon-holder div").addClass("bg-pink")
+        $("#mobile-nav-icon-holder div").removeClass("bg-beige")
+        isNavOpen = false
+    });
+})
