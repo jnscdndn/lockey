a61a1f769779906dd34cca775932db7a0396e08f
Jyotiranjan sankhua <jyoti.sankhua@gmail.com>
2024-11-01 10:57:54 +0530

    changes

--- js/hamburger.js
+++ js/hamburger.js
@@ -3,25 +3,23 @@ let menu=$("#nav-bar-contents-mobile")
 $("#mobile-nav-icon-holder").click(function(){
     if(isNavOpen){
         menu.slideToggle(function(){
-            $("#top-nav-bar-mobile").removeClass("bg-pink");
-            $("#mobile-nav-icon-holder div").addClass("bg-pink")
-            $("#mobile-nav-icon-holder div").removeClass("bg-beige")
+            console.log("false")
+            // $("#top-nav-bar-mobile").removeClass("bg-pink");
+            // $("#mobile-nav-icon-holder div").addClass("bg-pink")
+            // $("#mobile-nav-icon-holder div").removeClass("bg-beige")
             isNavOpen = false
         });
     }
     else{
         menu.slideToggle()
-        $("#top-nav-bar-mobile").addClass("bg-pink")
-        $("#mobile-nav-icon-holder div").removeClass("bg-pink")
-        $("#mobile-nav-icon-holder div").addClass("bg-beige")
+        console.log("true")
+        // $("#top-nav-bar-mobile").addClass("bg-pink")
+        // $("#mobile-nav-icon-holder div").removeClass("bg-pink")
+        // $("#mobile-nav-icon-holder div").addClass("bg-beige")
         isNavOpen = true
     }
 })
-menu.on('touchend',function(){
-    menu.slideToggle(function(){
-        $("#top-nav-bar-mobile").removeClass("bg-pink");
-        $("#mobile-nav-icon-holder div").addClass("bg-pink")
-        $("#mobile-nav-icon-holder div").removeClass("bg-beige")
-        isNavOpen = false
-    });
+menu.click(function(){
+    menu.slideToggle()
+    isNavOpen = false
 })
