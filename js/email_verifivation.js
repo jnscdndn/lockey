f561732ec35251cc4cb6e0514f9ed40a69c83f05
Pritish Prasant Sahoo <pps491415@gmail.com>
2024-12-05 23:44:14 +0530

    Added e mail base check

--- js/email_verifivation.js
+++ js/email_verifivation.js
@@ -1,13 +1,28 @@
 $("#send_otp").click(function(){
     let email=$("#email").val();
+    // const otp = getotp();
     $.ajax({
-        url:"",
+        url:"./../php/email_verification.php",
         method:"POST",
         data:{
             email:email
         },
         success:function(data){
-
+            data = JSON.parse(data);
+            if(data['status']=="success"){
+                $("#send_otp").text("Otp Sent");
+                $("#email").prop('disabled', true);
+            }
+            else{
+                console.log(data);
+                
+            }
+            
+            
         }
     })
 })
+
+function getotp(){
+    return 1234;
+}
