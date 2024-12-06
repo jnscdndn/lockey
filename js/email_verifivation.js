c60354c1c96367f762ba64a3f85b2920c8ea84c0
Pritish Prasant Sahoo <pps491415@gmail.com>
2024-12-06 21:45:30 +0530

    Nothing

--- js/email_verifivation.js
+++ js/email_verifivation.js
@@ -1,26 +1,34 @@
 $("#send_otp").click(function(){
     let email=$("#email").val();
-    // const otp = getotp();
-    $.ajax({
-        url:"./../php/email_verification.php",
-        method:"POST",
-        data:{
-            email:email
-        },
-        success:function(data){
-            data = JSON.parse(data);
-            if(data['status']=="success"){
-                $("#send_otp").text("Otp Sent");
-                $("#email").prop('disabled', true);
+    
+    if(email===""){
+        $("#email-error").text("Please enter an email");
+    }else if(!email.match(/^[a-zA-Z0-9.]{3,}@[a-zA-Z]{3,12}.[a-zA-Z]{2,5}$/)){
+        $("#email-error").text("Please enter a valid email");
+    } else{
+        $("#email-error").text("");
+        $.ajax({
+            url:"./../php/email_verification.php",
+            method:"POST",
+            data:{
+                email:email
+            },
+            success:function(data){
+                data = JSON.parse(data);
+                if(data['status']=="success"){
+                    console.log("HERE");
+                    
+                    $("#send_otp").text("Otp Sent");
+                    $("#email").prop('disabled', true);
+                }
+                else{
+                    console.log(data);
+                    
+                }
+ 
             }
-            else{
-                console.log(data);
-                
-            }
-            
-            
-        }
-    })
+        })
+    }
 })
 
 function getotp(){
