593404ddbcd31c705b3f1e1193f2efca5768de86
Pritish Prasant Sahoo <pps491415@gmail.com>
2024-12-10 23:01:59 +0530

    Completed Login and started Session

---
+++ js/login.js
@@ -0,0 +1,61 @@
+$("#login-form").submit((e)=>{
+    // e.preventDefault();
+    let email = $("#email").val();
+    let password = $("#password").val();
+    let iserror = false
+    if(email==""){
+        $('#email-error').text("Please enter your email");
+        iserror=true;
+    }else if(!email.match(/^[a-zA-Z0-9.]{3,}@[a-zA-Z]{3,12}.[a-zA-Z]{2,5}$/)){
+        $('#email-error').text("Please enter a valid email");
+        iserror=true;
+    }else{
+        $('#email-error').text("");
+    }
+
+    if(password==""){
+        $('#password-error').text("Please enter your password");
+        iserror=true;
+    }else if(!password.match(/[0-9]/) || !password.match(/[a-z]/) || !password.match(/[A-Z]/) || !password.match(/[@#$%^&*-_=+?/><]/)){
+        $('#password-error').text("Enter a valid password");
+        iserror=true;
+    }else if(password.length<8){
+        $('#password-error').text("Enter a valid password");
+    }else{
+        $('#password-error').text("");
+        var pass_hash = CryptoJS.SHA256(password).toString();
+        // console.log(pass_hash);
+        $("#encrypted_password").val(pass_hash);
+    }
+    if(iserror){
+        e.preventDefault();
+    } else{
+        $.ajax({
+            url:"./../php/login.php",
+            method:"post",
+            data:{email:email,password:pass_hash},
+            success:(data)=>{
+                try{
+                    data = JSON.parse(data);
+                    if(data['status']=="success"){
+                        console.log("Success");
+                        
+                    } else{
+                        if(data['invalid']=="email"){
+                            $("#email-error").text("Email is not registered");
+                        } else if(data['invalid']=="password"){
+                            $("#password-error").text("Invalid Password");
+                        }  
+                    }
+                }catch(Error){
+                    console.log(Error);
+                    console.log(data);
+                    
+                    
+                }
+            }
+            
+        })
+    }
+    
+})
