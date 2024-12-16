44e6fe6c9fa52a3cbfc122d7f11aca0e16d04931
Pritish Prasant Sahoo <pps491415@gmail.com>
2024-12-16 19:16:54 +0530

    Update page completed

--- js/update-password.js
+++ js/update-password.js
@@ -18,6 +18,7 @@ populateDetails=(element)=>{
     $("#update-password").removeClass("d-none");
     $("#update-password").addClass("d-block");
     let data=element;
+    $("#updated-id").val(data['id']);
     $("#updated-app-name").val(data['name']);
     $("#updated-url").val(data['url']);
     $("#updated-username").val(data['username']);
@@ -25,9 +26,43 @@ populateDetails=(element)=>{
     $("#updated-note").val(data['note']);
     if (data['favourite'] === "yes") {
         $('#updated-favourite').prop('checked', true); // Check the checkbox
-    } else if (value === "no") {
+    } else if ( data['favourite']=== "no") {
         $('#updated-favourite').prop('checked', false); // Uncheck the checkbox
     }
-
 }
 
+$("#update-password-form").submit((e)=>{
+    let appname = $("#updated-app-name").val();
+    let url = $("#updated-url").val();
+    let username = $("#updated-username").val();
+    let password = $("#updated-password").val();
+    let note = $("#updated-note").val();
+    let check = $("#updated-favourite");
+    let favourite = "no";
+    if(check.is(":checked")){
+        favourite="yes";
+    }
+    let isError=false;
+    if(appname==""){
+        $("#updated-app-name-error").text("Please enter the app name");
+        isError=true;
+    }else{
+        $("#updated-app-name-error").text("");
+    }
+    if(username==""){
+        $("#updated-username-error").text("Please enter the username");
+        isError=true;
+    }else{
+        $("#updated-username-error").text("");
+    }
+    if(password==""){
+        $("#updated-password-error").text("Please enter the password");
+        isError=true;
+    }else{
+        $("#updated-password-error").text("");
+        $("#updated-encrypted_password").val(encryptPassword(password,username))
+    }
+    if(isError){
+       e.preventDefault(); 
+    }
+})
